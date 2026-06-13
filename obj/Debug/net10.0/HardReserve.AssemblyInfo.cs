1106cc6bd80c79cbb0a29d7001132a3964e9f97b
Caio Henrique <caiohbdasilva@gmail.com>
2026-06-13 12:14:00 -0300

    Mudança de autenticação do BD para "Autenticação do Windows"

--- obj/Debug/net10.0/HardReserve.AssemblyInfo.cs
+++ obj/Debug/net10.0/HardReserve.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HardReserve")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+200eb4c9059e2cf92d3c48369917a52e2ffd26bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037ad54d281d3bbdcf1a915092a89ed805056fd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("HardReserve")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HardReserve")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
